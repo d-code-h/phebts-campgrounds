4ea7419dedfd9238613901d3b02bf3fd5e91ee3c
David Yunusa <daveed856@outlook.com>
2023-07-22 10:19:11 +0100

    Restructure to align with RESTFUl routing

---
+++ app/campgrounds/new/page.tsx
@@ -0,0 +1,7 @@
+export default function New() {
+  return (
+    <div>
+      <h1>Add new campground.</h1>
+    </div>
+  );
+}
---
+++ app/campgrounds/page.tsx
@@ -0,0 +1,9 @@
+import Jumbotron from '../components/Jumbotron';
+
+export default function Campgrounds() {
+  return (
+    <div>
+      <Jumbotron />
+    </div>
+  );
+}
