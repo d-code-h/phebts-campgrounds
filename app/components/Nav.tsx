e67713723ec6c751874ef272435997a7696dbf6e
d-code-h <74269728+d-code-h@users.noreply.github.com>
2023-07-22 18:28:50 +0100

    Add authentication (#13)

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,12 +1,28 @@
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server';
+
 export default function Nav() {
+  const { isAuthenticated } = getKindeServerSession();
   return (
     <nav>
-      <a
-        className="text-xl text-purple-500 font-semibold px-2 py-1 rounded-md ring-purple-500 hover:ring-2 focus:outline-none focus:ring-2"
-        href="#"
-      >
-        Sign in
-      </a>
+      {isAuthenticated() ? (
+        <LogoutLink className="text-xl text-purple-500 font-semibold px-2 py-1 rounded-md ring-purple-500 hover:ring-2 focus:outline-none focus:ring-2">
+          Log out
+        </LogoutLink>
+      ) : (
+        <>
+          <LoginLink className="text-xl text-purple-500 font-semibold px-2 py-1 rounded-md ring-purple-500 hover:ring-2 focus:outline-none focus:ring-2">
+            Sign in
+          </LoginLink>
+          <RegisterLink className="text-xl text-purple-500 font-semibold px-2 py-1 rounded-md ring-purple-500 hover:ring-2 focus:outline-none focus:ring-2">
+            Sign up
+          </RegisterLink>
+        </>
+      )}
     </nav>
   );
 }
