59f33b8000180dcfc7cdc7a243ab1207b1527dd3
David Yunusa <daveed856@outlook.com>
2023-07-22 18:25:31 +0100

    Add authentication

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
 import Nav from './Nav';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 export default function Header() {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
   return (
     <header className="flex justify-between items-center px-3 py-2 bg-slate-20 shadow-md shadow-purple-50">
       <h1 className="text-3xl font-bold text-purple-500">
         <Link href="/campgrounds">Phebts</Link>
       </h1>
+      {user && (
+        <h2 className="font-mono font-semibold text-xl text-purple-500 animate-bounce tracking-widest">
+          Welcome {user.given_name} 😊
+        </h2>
+      )}
       <Nav />
     </header>
   );
