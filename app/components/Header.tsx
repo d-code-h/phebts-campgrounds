6281df5f10fe6de022d9e49675fe936c220b01c8
David Yunusa <daveed856@outlook.com>
2023-07-22 10:34:15 +0100

    Link brand to homepage

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import Nav from './Nav';
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-3 py-2 bg-slate-20 shadow-md shadow-purple-50">
-      <h1 className="text-3xl font-bold text-purple-500">Phebts</h1>
+      <h1 className="text-3xl font-bold text-purple-500">
+        <Link href="/campgrounds">Phebts</Link>
+      </h1>
       <Nav />
     </header>
   );
